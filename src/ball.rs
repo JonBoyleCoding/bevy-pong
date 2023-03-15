032abf78d6e56d1adda1ed754be23e87fd7fd76e
JonBoyleCoding <programmerfalcon@gmail.com>
2023-03-15 23:29:49 +0000

    Cargo FMT

--- src/ball.rs
+++ src/ball.rs
@@ -2,27 +2,27 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Ball {
-    pub velocity: Vec2,
-    pub radius: f32,
-    pub position: Vec2,
+	pub velocity: Vec2,
+	pub radius: f32,
+	pub position: Vec2,
 }
 
 impl Default for Ball {
-    fn default() -> Self {
-        Ball {
-            velocity: Vec2::new(0.0, 0.0),
-            radius: 0.0,
-            position: Vec2::new(0.0, 0.0),
-        }
-    }
+	fn default() -> Self {
+		Ball {
+			velocity: Vec2::new(0.0, 0.0),
+			radius: 0.0,
+			position: Vec2::new(0.0, 0.0),
+		}
+	}
 }
 
 impl Ball {
-    pub fn new(radius: f32, position: Vec2) -> Self {
-        Ball {
-            radius,
-            position,
-            ..Default::default()
-        }
-    }
+	pub fn new(radius: f32, position: Vec2) -> Self {
+		Ball {
+			radius,
+			position,
+			..Default::default()
+		}
+	}
 }
--- src/paddle.rs
+++ src/paddle.rs
@@ -4,46 +4,42 @@ use getset::{Getters, Setters};
 /// The paddle side enum. This enum is used to keep track of which side the paddle is on.
 #[derive(Eq, PartialEq, Debug, Copy, Clone, Component)]
 pub enum PaddleSide {
-    Left,
-    Right,
+	Left,
+	Right,
 }
 
 /// The paddle component. This component is used to keep track of the paddle's position, size, and side.
 #[derive(Component, Getters, Setters)]
 pub struct Paddle {
-    #[getset(get = "pub", set = "pub")]
-    y_pos: f32,
-    #[getset(get = "pub", set = "pub")]
-    size: f32,
+	#[getset(get = "pub", set = "pub")]
+	y_pos: f32,
+	#[getset(get = "pub", set = "pub")]
+	size: f32,
 }
 
 impl Default for Paddle {
-    fn default() -> Self {
-        Paddle {
-            y_pos: 0.0,
-            size: 0.0,
-        }
-    }
+	fn default() -> Self {
+		Paddle { y_pos: 0.0, size: 0.0 }
+	}
 }
 
 impl Paddle {
-    pub fn new(size: f32) -> Self {
-        Paddle {
-            size,
-            ..Default::default()
-        }
-    }
+	pub fn new(size: f32) -> Self {
+		Paddle {
+			size,
+			..Default::default()
+		}
+	}
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn paddle_new() {
-        let paddle = Paddle::new(10.0);
-        assert_eq!(*paddle.size(), 10.0);
-        assert_eq!(*paddle.y_pos(), 0.0);
-    }
-
+	#[test]
+	fn paddle_new() {
+		let paddle = Paddle::new(10.0);
+		assert_eq!(*paddle.size(), 10.0);
+		assert_eq!(*paddle.y_pos(), 0.0);
+	}
 }
